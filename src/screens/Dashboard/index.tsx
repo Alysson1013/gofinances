231d7ea465367e73589aba529df665e57800d621
Alysson <alysson6801@hotmail.com>
2021-08-24 22:28:26 -0300

    chore: creating a styled component

---
+++ src/screens/Dashboard/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { StyleSheet } from 'react-native';
+import { View, Text } from 'react-native';
+import { Container, Title } from './styles'
+
+export function Dashboard(){
+  return (
+    <Container>
+      <Title>
+        Dashboard
+      </Title>
+    </Container>
+  )
+};
