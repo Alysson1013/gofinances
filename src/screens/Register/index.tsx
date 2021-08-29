5557862944b604a777f85bfb63c2034b575e3ac0
Alysson <alysson6801@hotmail.com>
2021-08-29 12:58:49 -0300

    chore: add button component

--- src/screens/Register/index.tsx
+++ src/screens/Register/index.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Input } from '../../components/Form/Input';
+import { Button } from '../../components/Form/Button';
 
 import {
   Container,
   Header,
   Title,
-  Form
+  Form,
+  Fields
 } from './styles'
 
 export function Register() {
@@ -15,12 +17,17 @@ export function Register() {
         <Title>Cadastro</Title>
       </Header>
       <Form>
-        <Input
-          placeholder="Nome"
-        />
-        <Input
-          placeholder="Preço"
-        />
+        <Fields>
+          <Input
+            placeholder="Nome"
+          />
+          <Input
+            placeholder="Preço"
+          />
+        </Fields>
+
+
+        <Button title="Enviar" />
       </Form>
     </Container>
   )
