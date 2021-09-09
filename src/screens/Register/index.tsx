6f1bf20f0370c78216368ffc7c63c8f077f3e127
Alysson <alysson6801@hotmail.com>
2021-09-09 12:08:13 -0300

    chore: remove async storage items

--- src/screens/Register/index.tsx
+++ src/screens/Register/index.tsx
@@ -83,7 +83,7 @@ export function Register() {
     if(category.key === 'category')
       return Alert.alert('Selecione a categoria da transação')
 
-    const data = {
+    const newTransaction = {
       name: form.name,
       amount: form.amount,
       transactionType,
@@ -91,7 +91,15 @@ export function Register() {
     }
 
     try {
-      await AsyncStorage.setItem(dataKey, JSON.stringify(data))
+      const data = await AsyncStorage.getItem(dataKey)
+      const currentData = data ? JSON.parse(data) : []
+
+      const dataFormatted = [
+        ...currentData,
+        newTransaction
+      ]
+
+      await AsyncStorage.setItem(dataKey, JSON.stringify(dataFormatted))
 
     } catch(error) {
       console.log(error)
